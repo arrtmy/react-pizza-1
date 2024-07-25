26236b09ea8e09b2c650d62d4671ca34b18a9d99
arrtmy <159881749+arrtmy@users.noreply.github.com>
2024-07-25 17:03:29 +1200

    res
    res, skeleton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import { React } from 'react';
+import React, { useState, useEffect } from 'react';
 import './scss/app.scss';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/pizzas.json';
+import PizzaBlock from './components/PizzaList/PizzaBlock';
+import Skeleton from './components/PizzaList/Skeleton';
 
 const App = () => {
+  const [pizzas, setPizzas] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://66a1c0fa967c89168f1d8196.mockapi.io/pizzas')
+      .then((res) => res.json())
+      .then((arr) => {
+        setPizzas(arr);
+        setIsLoading(false)
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-            {/* <Categories /> */}
+            <Categories />
             <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
+            {isLoading
+              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+              : pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
           </div>
         </div>
       </div>
---
+++ src/components/PizzaList/Skeleton.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+const Skeleton = () => (
+  <ContentLoader 
+    className="pizza-block"
+    speed={2}
+    width={280}
+    height={465}
+    viewBox="0 0 280 465"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb">
+    <rect x="2" y="277" rx="10" ry="10" width="279" height="23" /> 
+    <rect x="2" y="326" rx="10" ry="10" width="278" height="88" /> 
+    <circle cx="137" cy="129" r="128" /> 
+    <rect x="5" y="428" rx="15" ry="15" width="97" height="29" /> 
+    <rect x="139" y="421" rx="20" ry="20" width="137" height="43" />
+  </ContentLoader>
+)
+
+export default Skeleton
+
